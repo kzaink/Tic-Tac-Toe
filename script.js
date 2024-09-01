fdb6b60e9860646a81832b08e228209f11c1b222
kzaink <zainulaabdeenkhan123@gmail.com>
2024-08-31 20:23:47 -0500

    completed files

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ let enableBoxes = () => {
 };
 
 const showWinner = (winner) => {
-  msg.innerText = `Congratulations , Winner is ${winner}`;
+  msg.innerText = `Congratulations , Winner is Player"${winner}"`;
   msgContainer.classList.remove("hidden");
   disableBoxes();
 };
